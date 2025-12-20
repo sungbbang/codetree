9a6ac1bee8d22e09d756405312c3f54a655af010
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-20 21:35:05 +0900

    Title: 0 나올때까지 출력, Time: 74ms, Memory: 10MB, Status: Passed - Codetree

--- 251220/0 나올때까지 출력/print-until-zero-is-given.js
+++ 251220/0 나올때까지 출력/print-until-zero-is-given.js
@@ -3,7 +3,7 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 let idx = 0;
 while (true) {
-    num = Number(input[idx++]);
+    let num = Number(input[idx++]);
     if (num === 0) {
         break;
     }
