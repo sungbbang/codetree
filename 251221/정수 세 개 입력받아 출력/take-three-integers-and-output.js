449ab7310af636fdf908a1984e298c5bc46a6790
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-21 10:37:39 +0900

    Title: 정수 세 개 입력받아 출력, Time: 62ms, Memory: 10MB, Status: Passed - Codetree

--- 251221/정수 세 개 입력받아 출력/take-three-integers-and-output.js
+++ 251221/정수 세 개 입력받아 출력/take-three-integers-and-output.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
-const [a, b, c] = fs.readFileSync(0).toString().split('\n').map(Number);
+const input = fs.readFileSync(0).toString().split('\n')
+
+const [a, b] = input[0].split(' ').map(Number);
+const c = Number(input[1]);
 
 console.log(a, b, c);
