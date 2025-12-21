bb15547d6ecb9e35f113add4f6124d80fdc68fc7
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-21 17:44:51 +0900

    Title: 세 정수의 최솟값, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 251221/세 정수의 최솟값/minimum-of-three-numbers.js
+++ 251221/세 정수의 최솟값/minimum-of-three-numbers.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const [a, b, c] = fs.readFileSync(0).toString().split(' ').map(Number);
 
-if (a < b && a < c) {
+if (a <= b && a <= c) {
     console.log(a);
-} else if (b < a && b < c) {
+} else if (b <= a && b <= c) {
     console.log(b);
 } else {
     console.log(c);
