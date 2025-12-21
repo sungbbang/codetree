939a97f83513524a988d2ebe2bddc31805c36d4a
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-22 08:57:37 +0900

    Title: 100점 도달하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251222/100점 도달하기/reach-100-point.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+grade = 'F'
+while n < 100:
+    if n >= 90: grade = 'A'
+    elif n >= 80: grade = 'B'
+    elif n >= 70: grade = 'C'
+    elif n >= 60: grade = 'D'
+    print(grade, end=' ')
+    n += 1
