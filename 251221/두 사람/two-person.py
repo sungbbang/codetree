b343860e7f52a71f2ef277a1d4a7a580eff8d8c9
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-21 18:16:26 +0900

    Title: 두 사람, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251221/두 사람/two-person.py
+++ 251221/두 사람/two-person.py
@@ -1,7 +1,7 @@
 p1 = input().split()
 p2 = input().split()
 
-if (int(p1[0]) >= 19 and p1[1] == 'M') or (int(p1[0]) >= 19 and p1[1] == 'M'):
+if (int(p1[0]) >= 19 and p1[1] == 'M') or (int(p2[0]) >= 19 and p2[1] == 'M'):
     print(1)
 else:
     print(0)
