3f62fba40975ca661832c5d38a115559c1a32d5b
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-21 10:36:21 +0900

    Title: 세 실수의 반올림, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251221/세 실수의 반올림/rounding-of-three-actual-numbers.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const [a, b, c] = fs.readFileSync(0).toString().split('\n').map(Number);
+
+console.log(a.toFixed(3));
+console.log(b.toFixed(3));
+console.log(c.toFixed(3));
