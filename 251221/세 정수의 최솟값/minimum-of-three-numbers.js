e43b003556bf801dc0467bb20990874784833d06
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-21 17:44:05 +0900

    Title: 세 정수의 최솟값, Time: 66ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251221/세 정수의 최솟값/minimum-of-three-numbers.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const [a, b, c] = fs.readFileSync(0).toString().split(' ').map(Number);
+
+if (a < b && a < c) {
+    console.log(a);
+} else if (b < a && b < c) {
+    console.log(b);
+} else {
+    console.log(c);
+}
