34967661db2832f35d3f5e1b1bf032b9a172399e
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-22 15:00:51 +0900

    Title: 별표 출력하기 8, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251222/별표 출력하기 8/print-star-8.py
+++ 251222/별표 출력하기 8/print-star-8.py
@@ -4,5 +4,6 @@ for i in range(1, n+1):
     if i % 2 == 1:
         print('*')
     else:
-        for _ in range(i * 2):
+        for _ in range(i):
             print('*', end=' ')
+        print()
