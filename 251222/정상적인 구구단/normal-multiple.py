5975c7394c6ed42e0c3982e04cf2be3e36caf834
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-22 20:44:13 +0900

    Title: 정상적인 구구단, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251222/정상적인 구구단/normal-multiple.py
+++ 251222/정상적인 구구단/normal-multiple.py
@@ -3,6 +3,6 @@ n = int(input())
 for i in range(1, n+1):
     for j in range(1, n+1):
         print(f'{i} * {j} = {i*j}', end='')
-        if j <= n:
+        if j < n:
             print(',', end=' ')
     print()
