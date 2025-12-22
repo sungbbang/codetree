4900067b255235be0fca3375503fe7f09d7549aa
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-22 15:12:56 +0900

    Title: 별표 출력하기 13, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251222/별표 출력하기 13/print-start-13.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for i in range(n):
+    for _ in range(n-i, 0, -1):
+        print('*', end=' ')
+    print()
+
+    for _ in range(i+1):
+        print('*', end=' ')
+    print()
