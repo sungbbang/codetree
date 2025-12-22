9f5a8effaaffb4969696ad7627403b270c2431be
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-22 14:00:12 +0900

    Title: 별표 출력하기 9, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251222/별표 출력하기 9/print-star-9.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for i in range(n):
+    for _ in range((n-1-i)*2):
+        print(' ', end='')
+    
+    for _ in range(2 * i + 1):
+        print('*', end=' ')
+    
+    print()
