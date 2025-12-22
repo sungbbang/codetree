2b44de0c0efdcac6d18014fa416fd76a2462a5ac
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-22 09:33:35 +0900

    Title: 369 게임, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251222/369 게임/369-game.py
+++ 251222/369 게임/369-game.py
@@ -2,18 +2,20 @@ n = int(input())
 
 i = 1
 while i <= n:
+    # 3의 배수일 때
     if i % 3 == 0:
         print(0, end=' ')
+    
+    # 3의 배수가 아닐 때
     else:
+        # 3, 6, 9가 들어가는지 판별
         num = i
         flag = False
-        while num > 0:
+        while not flag and num > 0:
             mod = num % 10
-            if mod == 3 or mod == 6 or mod == 9:
-                print(0, end=' ')
-                flag = True
-                break
             num //= 10
+            if mod == 3 or mod == 6 or mod == 9:
+                flag = True
         
         if flag: print(0, end=' ')
         else: print(i, end=' ')
