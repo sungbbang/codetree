db4b6a7c68807646cfa971e190aecefe29080b94
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-22 14:24:07 +0900

    Title: 별표 출력하기 6, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 251222/별표 출력하기 6/print-star-6.py
+++ 251222/별표 출력하기 6/print-star-6.py
@@ -7,5 +7,14 @@ for i in range(n):
     # 별
     for _ in range((n-i)*2-1):
         print('*', end=' ')
-    
     print()
+
+for i in range(n - 2, -1, -1):
+    # 공백
+    for _ in range(i * 2):
+        print(' ', end='')
+    # 별
+    for _ in range((n-i)*2-1):
+        print('*', end=' ')
+    print()
+    
