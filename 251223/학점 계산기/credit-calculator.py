50765f545eee3222a8128a6423b03658bcbb8c2d
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-23 10:54:09 +0900

    Title: 학점 계산기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251223/학점 계산기/credit-calculator.py
+++ 251223/학점 계산기/credit-calculator.py
@@ -2,7 +2,7 @@ n = int(input())
 arr = list(map(float, input().split()))
 
 avg = sum(arr) / len(arr)
-print(avg)
+print(f'{avg:.1f}')
 
 if avg >= 4.0: print('Perfetct')
 elif avg >= 3.0: print('Good')
