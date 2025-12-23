ee3f750ae7d265edc9d39971f77182f35e2a647b
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-24 00:23:48 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251224/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 251224/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -3,13 +3,13 @@ a = list(map(int, input().split()))
 
 # Please write your code here.
 mx_idx = n
-mx = 0
 ans = []
 while mx_idx != 0:
+    mx = 0
     for i in range(mx_idx):
         if a[i] > mx:
             mx = a[i]
-            mx_idx = i+1
+            mx_idx = i
     ans.append(mx_idx)
 
-print(mx_idx)
+print(ans)
