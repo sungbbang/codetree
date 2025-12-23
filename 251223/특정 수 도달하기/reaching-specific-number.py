6f5a85cd3d9e106e56c9c51198aa9b7261733634
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-23 10:49:03 +0900

    Title: 특정 수 도달하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251223/특정 수 도달하기/reaching-specific-number.py
+++ 251223/특정 수 도달하기/reaching-specific-number.py
@@ -1,12 +1,13 @@
 arr = list(map(int, input().split()))
 sum_ = 0
-
+ans = []
 for el in arr:
     if el >= 250:
         break
     
     sum_ += el
+    ans.append(el)
 
-avg = sum_ / len(arr)
+avg = sum_ / len(ans)
 
 print(f'{sum_} {avg:.1f}')
