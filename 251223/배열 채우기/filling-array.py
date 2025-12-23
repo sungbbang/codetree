786aee148c2bc1e225678f01b7e14cd12e1f1895
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-23 11:09:43 +0900

    Title: 배열 채우기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251223/배열 채우기/filling-array.py
+++ 251223/배열 채우기/filling-array.py
@@ -1,9 +1,9 @@
 arr = list(map(int, input().split()))
 
-zero = len(arr) - 1
+zero = len(arr)
 for i in range(len(arr)):
     if arr[i] == 0:
         zero = i
         break
 
-print(*arr[zero-1::-1])
+print(*arr[:zero][::-1])
