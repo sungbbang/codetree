6ccf6aa04b1e393217f8fd564da2ae83314662dd
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-23 09:49:29 +0900

    Title: 알파벳 출력하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251223/알파벳 출력하기/print-alphabet.py
+++ 251223/알파벳 출력하기/print-alphabet.py
@@ -5,5 +5,9 @@ idx = ord('A')
 for i in range(n):
     for j in range(i+1):
         print(chr(idx), end='')
-        idx += 1
+        if chr(idx) == 'Z':
+            idx = ord('A')
+        else:
+            idx += 1
+
     print()
