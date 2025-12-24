cc9d986cebbe2c86962b9887c764a25539ad657e
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-24 23:21:00 +0900

    Title: 문자열 놀이, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251224/문자열 놀이/play-with-string.py
+++ 251224/문자열 놀이/play-with-string.py
@@ -1,15 +1,24 @@
 s, q = input().split()
-
 for _ in range(int(q)):
     c, a, b = input().split()
     c = int(c)
+    temp = ''
 
     if c == 1:
         a, b = int(a)-1, int(b)-1
-        s = s[:a] + s[b] + s[a+1:b] + s[a] + s[b+1:]
+        for i in range(len(s)):
+            if i == a:
+                temp += s[b]
+            elif i == b:
+                temp += s[a]
+            else:
+                temp += s[i]
     else:
         for i in range(len(s)):
             if s[i] == a:
-                s = s[:i] + b + s[i+1:]
+                temp += b
+            else:
+                temp += s[i]
     
+    s = temp
     print(s)
