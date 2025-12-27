bed5e4708b7e8cf192aecc008667f5fa18b976c0
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-27 17:46:14 +0900

    Title: 사칙연산 함수, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251227/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,27 @@
+a, o, c = input().split()
+a = int(a)
+c = int(c)
+
+# Please write your code here.
+def add(a, b):
+    return a + b
+
+def minus(a, b):
+    return a - b
+
+def divide(a, b):
+    return a / b
+
+def multiple(a, b):
+    return a * b
+
+if o == '+':
+    print(f'{a} {o} {c} = {add(a, c)}')
+elif o == '-':
+    print(f'{a} {o} {c} = {minus(a, c)}')
+elif o == '*':
+    print(f'{a} {o} {c} = {multiple(a, c)}')
+elif o == '/':
+    print(f'{a} {o} {c} = {divide(a, c)}')
+else:
+    print(False)
