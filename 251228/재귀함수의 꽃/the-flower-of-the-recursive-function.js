e88502a0b13b066157c9cb177dc74f2000515a7e
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-28 10:35:49 +0900

    Title: 재귀함수의 꽃, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251228/재귀함수의 꽃/the-flower-of-the-recursive-function.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString());
+
+function recursion(n) {
+    if (n === 0) return;
+
+    process.stdout.write(n + ' ');
+    recursion(n-1);
+    process.stdout.write(n + ' ');
+}
+
+recursion(n);
