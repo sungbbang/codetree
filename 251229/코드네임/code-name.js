a520990335b3a13ae880ba2907c6ee10841cf330
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-29 16:05:47 +0900

    Title: 코드네임, Time: 66ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251229/코드네임/code-name.js
@@ -0,0 +1,24 @@
+class Agent {
+    constructor(codeName = 0, score = 0) {
+        this.codeName = codeName;
+        this.score = score;
+    }
+}
+
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const agentList = [];
+for (let i = 0; i < 5; i++) {
+    const [codeName, score] = input[i].split(' ');
+    agentList.push(new Agent(codeName, Number(score)));
+}
+
+min = agentList[0];
+for (let i = 0; i < 5; i++) {
+    if (min <= agentList[i].score) {
+        min = agentList[i];
+    }
+}
+
+console.log(min.codeName, min.score);
