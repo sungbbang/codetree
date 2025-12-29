d68f91c4aa43a3cb2f1ae09427debc3926645c80
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-29 22:41:28 +0900

    Title: 정렬된 수 위치 알아내기, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 251229/정렬된 수 위치 알아내기/indices-of-sorted-array.py
+++ 251229/정렬된 수 위치 알아내기/indices-of-sorted-array.py
@@ -4,10 +4,8 @@ nums = [(num, i) for i, num in enumerate(arr)]
 ans = [0] * n
 
 nums.sort(key=lambda x: (x[0], x[1]))
-print(nums)
 
-
-for i, num in enumerate(nums):
-    ans[num[1]] = i + 1
+for i, num in enumerate(nums, start=1):
+    ans[num[1]] = i
 
 print(*ans)
