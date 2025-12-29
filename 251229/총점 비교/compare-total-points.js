65533b766173ed7b7f897b6d34e66df0a9126b1d
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-29 20:20:23 +0900

    Title: 총점 비교, Time: 76ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251229/총점 비교/compare-total-points.js
@@ -0,0 +1,21 @@
+class Person {
+    constructor(name, sub1, sub2, sub3) {
+        this.name = name;
+        this.sub1 = sub1;
+        this.sub2 = sub2;
+        this.sub3 = sub3;
+    }
+}
+
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+
+const arr = [];
+for (let i = 1; i <= n; i++) {
+    const [name, sub1, sub2, sub3] = input[i].split(' ');
+    arr.push(new Person(name, Number(sub1), Number(sub2), Number(sub3)));
+}
+
+arr.sort((a, b) => (a.sub1 + a.sub2 + a.sub3) - (b.sub1 + b.sub2 + b.sub3));
+arr.forEach(el => console.log(el.name, el.sub1, el.sub2, el.sub3));
