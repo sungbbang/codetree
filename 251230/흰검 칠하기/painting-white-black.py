1fdda63e61b733fecb12635a4be6f8a816b463e2
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-30 19:38:27 +0900

    Title: 흰검 칠하기, Time: 88ms, Memory: 22MB, Status: Passed - Codetree

--- 251230/흰검 칠하기/painting-white-black.py
+++ 251230/흰검 칠하기/painting-white-black.py
@@ -3,30 +3,30 @@ commands = [input().split() for _ in range(n)]
 
 MAX = n * 100
 arr = [''] * (2*MAX+1)
-curr = 0
+curr = MAX
 for x, dr in commands:
     x = int(x)
-    next_pos = curr - x if dr == 'L' else curr + x
-    a, b = min(curr, next_pos), max(curr, next_pos)
+    if dr == 'L':
+        for i in range(x):
+            arr[curr - i] += 'w'
+        curr = curr - x + 1
+    else:
+        for i in range(x):
+            arr[curr + i] += 'b'
+        curr = curr + x - 1
 
-    for i in range(a+MAX, b+MAX):
-        if dr == 'L':
-            arr[i] += 'w'
-        else:
-            arr[i] += 'b'
-
-    curr = next_pos
-
-white, black, gray = 0, 0, 0
+w = b = g = 0
 for el in arr:
     if el == '':
         continue
 
-    if len(el) >= 4:
-        gray += 1
+    if el.count('w') >= 2 and el.count('b') >= 2:
+        g += 1
+    
     elif el[-1] == 'w':
-        white += 1
-    elif el[-1] == 'b':
-        black += 1
-
-print(white, black, gray)
+        w += 1
+    
+    else:
+        b += 1
+    
+print(w, b, g)
