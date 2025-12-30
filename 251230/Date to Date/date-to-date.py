5e3a81767b6a9360249a788ef1c24d1c4900b7c9
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-30 09:53:56 +0900

    Title: Date to Date, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251230/Date to Date/date-to-date.py
+++ 251230/Date to Date/date-to-date.py
@@ -1,5 +1,5 @@
 m1, d1, m2, d2 = map(int, input().split())
 
-num_of_days = [0, 31, 28, 31, 30, 31, 60, 31, 31, 30, 31, 30, 31]
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 print(sum(num_of_days[:m2]) + d2 - (sum(num_of_days[:m1]) + d1) + 1)
