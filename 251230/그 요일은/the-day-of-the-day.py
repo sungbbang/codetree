5098149073dd49166e5f95f41d04141d5dbf7a1e
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-30 10:45:30 +0900

    Title: 그 요일은, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251230/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,16 @@
+m1, d1, m2, d2 = map(int, input().split())
+A = input()
+
+num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+day_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+start_total_date = sum(num_of_days[:m1]) + d1
+end_total_date = sum(num_of_days[:m2]) + d2
+
+
+diff = end_total_date - start_total_date
+end_day = day_of_week[diff % 7]
+
+print(start_total_date)
+print(end_total_date)
+print(end_day)
