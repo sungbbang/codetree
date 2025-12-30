3d85be5c14781e352f03ccb949b29882e9a0539f
sungbbang <96762809+sungbbang@users.noreply.github.com>
2025-12-30 11:04:44 +0900

    Title: 그 요일은, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251230/그 요일은/the-day-of-the-day.py
+++ 251230/그 요일은/the-day-of-the-day.py
@@ -7,12 +7,9 @@ day_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 start_total_date = sum(num_of_days[:m1]) + d1
 end_total_date = sum(num_of_days[:m2]) + d2
 
-
 diff = end_total_date - start_total_date
-end_day = day_of_week[diff % 7]
-
 ans = diff // 7
-if A == day_of_week[diff % 7]:
+if day_of_week.index(A) <= diff % 7:
     ans += 1
 
 print(ans)
