b90f7dcdaf848cfc6fdc552b49b138038baa9548
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-01 16:44:49 +0900

    Title: 연속되는 수 2, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 260101/연속되는 수 2/continuous-number2.js
+++ 260101/연속되는 수 2/continuous-number2.js
@@ -3,15 +3,17 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const arr = input.slice(1, n+1).map(Number);
 
-let max = 1
-let cnt = 1
+let max = 1;
+let cnt = 0;
 for (let i = 0; i < n; i++) {
     if (i == 0 || arr[i-1] === arr[i]) {
         cnt++;
     } else {
-        max = max <= cnt ? cnt : max;
+        if (cnt >= max) max = cnt;
         cnt = 1
     }
 }
+
+if (cnt >= max) max = cnt;
 
 console.log(max);
