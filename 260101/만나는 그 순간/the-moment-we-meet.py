3024ddab9a1ccc8bffa712ed621450bd6c778c34
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-01 22:17:11 +0900

    Title: 만나는 그 순간, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260101/만나는 그 순간/the-moment-we-meet.py
+++ 260101/만나는 그 순간/the-moment-we-meet.py
@@ -2,7 +2,7 @@ n, m = map(int, input().split());
 a_dt = [tuple(input().split()) for _ in range(n)]
 b_dt = [tuple(input().split()) for _ in range(m)]
 
-MAX = 1000
+MAX = 10
 a = [0] * (2*MAX+1)
 b = [0] * (2*MAX+1)
 
@@ -10,27 +10,12 @@ curr = 0
 for d, t in a_dt:
     t = int(t)
     if d == 'R':
-        for i in range(curr+MAX, curr+MAX+t+1):
-            a[i] = i
-            curr += 1
+        for i in range(curr+1+MAX, curr+1+t+MAX):
+            a[i] = a[i-1] + 1            
     else:
-        for i in range(curr+MAX, curr+MAX+t+1):
-            a[curr-i] = i
-            curr += 1
+        for i in range(curr+1+MAX, curr+1+t+MAX):
+            a[i] = a[i-1] - 1
+    curr += t
 
-for d, t in b_dt:
-    t = int(t)
-    if d == 'R':
-        for i in range(curr+MAX, curr+MAX+t+1):
-            b[i] = i
-            curr += 1
-    else:
-        for i in range(curr+MAX, curr+MAX+t+1):
-            b[curr-i] = i
-            curr += 1
-
-for i in range(1, 2*MAX+1):
-    if a[i] == b[i]:
-        print(i)
-        break
-
+print(a)
+print(b)
