2fceb593acdce38ec0cb6aa20847761c1dc1e3eb
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-01 22:27:53 +0900

    Title: 만나는 그 순간, Time: 111ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 260101/만나는 그 순간/the-moment-we-meet.py
+++ 260101/만나는 그 순간/the-moment-we-meet.py
@@ -2,7 +2,7 @@ n, m = map(int, input().split());
 a_dt = [tuple(input().split()) for _ in range(n)]
 b_dt = [tuple(input().split()) for _ in range(m)]
 
-MAX = 10
+MAX = 1000
 a = [0] * (2*MAX+1)
 b = [0] * (2*MAX+1)
 
@@ -17,5 +17,27 @@ for d, t in a_dt:
             a[i] = a[i-1] - 1
     curr += t
 
-print(a)
-print(b)
+curr = 0
+for d, t in b_dt:
+    t = int(t)
+    if d == 'R':
+        for i in range(curr+1+MAX, curr+1+t+MAX):
+            b[i] = b[i-1] + 1            
+    else:
+        for i in range(curr+1+MAX, curr+1+t+MAX):
+            b[i] = b[i-1] - 1
+    curr += t
+
+
+second = 0
+flag = False
+for i in range(MAX, 2*MAX+1):
+    if a[i] != 0 and b[i] != 0 and a[i] == b[i]:
+        second = i
+        flag = True
+        break
+
+if not flag:
+    print(-1)
+else:
+    print(second - MAX)
