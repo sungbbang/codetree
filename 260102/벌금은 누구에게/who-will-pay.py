9e0aadd6d77dab4f259f671b6658192945af1670
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-02 16:51:37 +0900

    Title: 벌금은 누구에게, Time: 65ms, Memory: 17MB, Status: Passed - Codetree

--- 260102/벌금은 누구에게/who-will-pay.py
+++ 260102/벌금은 누구에게/who-will-pay.py
@@ -2,8 +2,14 @@ n, m, k = map(int, input().split())
 nums = [int(input()) for _ in range(m)]
 
 arr = [0] * (n+1)
+
+flag = False
 for num in nums:
     arr[num] += 1
     if arr[num] >= k:
+        flag = True
         print(num)
         break
+
+if not flag:
+    print(-1)
