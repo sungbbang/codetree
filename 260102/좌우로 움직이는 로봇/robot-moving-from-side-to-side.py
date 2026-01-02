8f35141d47bc3d70f92b6d3d76d5c4f35d5371b7
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-02 23:20:33 +0900

    Title: 좌우로 움직이는 로봇, Time: 169ms, Memory: 50MB, Status: Passed - Codetree

--- 260102/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 260102/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -6,40 +6,30 @@ B = [0] * (2*MAX+1)
 time_a = 1
 for _ in range(n):
     t, d = input().split()
-    t = int(t)
-    if d == 'R':
-        for _ in range(t):
-            A[time_a] = A[time_a - 1] + 1
-            time_a += 1
-    else:
-        for _ in range(t):
-            A[time_a] = A[time_a - 1] - 1
-            time_a += 1
+    for _ in range(int(t)):
+        A[time_a] = A[time_a - 1] + (1 if d == 'R' else -1)
+        time_a += 1
 
 time_b = 1
 for _ in range(m):
     t, d = input().split()
-    t = int(t)
-    if d == 'R':
-        for _ in range(t):
-            B[time_b] = B[time_b - 1] + 1
-            time_b += 1
-    else:
-        for _ in range(t):
-            B[time_b] = B[time_b - 1] - 1
-            time_b += 1
+    for _ in range(int(t)):
+        B[time_b] = B[time_b - 1] + (1 if d == 'R' else -1)
+        time_b += 1
 
 if time_a > time_b:
     for i in range(time_b, time_a):
-        B[i] = B[time_b]
+        B[i] = B[time_b - 1]
 elif time_a < time_b:
     for i in range(time_a, time_b):
-        A[i] = A[time_a]
+        A[i] = A[time_a - 1]
 
+
+max_time = max(time_a, time_b)
 
 cnt = 0
-for i in range(2*MAX+1):
-    if i >= 1 and A[i-1] != B[i-1] and A[i] == B[i]:
+for i in range(1, max_time):
+    if A[i-1] != B[i-1] and A[i] == B[i]:
         cnt += 1
 
 print(cnt)
