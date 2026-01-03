416412f7b6f642c54d2115d3061866975ed4a111
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-03 14:13:09 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260103/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 260103/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,27 +1,45 @@
-n, k, p, t = map(int, input().split())
-handshakes = [tuple(map(int, input().split())) for _ in range(t)]
-handshakes.sort(key=lambda x: x[0])
+# 클래스 선언
+class Shake:
+    def __init__(self, time, person1, person2):
+        self.time, self.person1, self.person2 = time, person1, person2
 
-devs = [0] * (n+1)
-devs[p] = 1
+# 변수 선언 및 입력
+n, k, p, t = tuple(map(int, input().split()))	
+shakes = []
+for _ in range(t):
+    time, person1, person2 = tuple(map(int, input().split()))
+    shakes.append(Shake(time, person1, person2))
 
-cnt = [0] * (n+1)
-for t, a, b in handshakes:
-    # 둘다 감염자인 경우
-    if devs[a] == 1 and devs[b] == 1:
-        if cnt[a] < k:
-            cnt[a] += 1
-        if cnt[b] < k:
-            cnt[b] += 1
-    # 한명만 감염자인 경우
-    if devs[a] == 1:
-        if cnt[a] < k:
-            devs[b] = 1
-            cnt[a] += 1
-    elif devs[b] == 1:
-        if cnt[b] < k:
-            devs[a] = 1
-            cnt[b] += 1
+shake_num = [0] * (n + 1)
+infected = [False] * (n + 1)
 
-for el in devs[1:]:
-    print(el, end='')
+infected[p] = True
+
+# Custom Comparator를 활용한 정렬
+shakes.sort(key = lambda x: x.time)
+
+# 각 악수 횟수를 세서,
+# K번 초과로 악수를 했을 시 전염시키지 않습니다.
+for shake in shakes:
+	target1 = shake.person1
+	target2 = shake.person2
+	
+	# 감염되어 있을 경우 악수 횟수를 증가시킵니다.
+	if infected[target1]:
+		shake_num[target1] += 1
+	if infected[target2]:
+		shake_num[target2] += 1
+	
+	# target1이 감염되어 있고 아직 K번 이하로 악수했다면 target2를 전염시킵니다.
+	if shake_num[target1] <= k and infected[target1]:
+		infected[target2] = True
+	
+	# target2가 감염되어 있고 아직 K번 이하로 악수했다면 target1을 전염시킵니다.
+	if shake_num[target2] <= k and infected[target2]:
+		infected[target1] = True
+		
+for i in range(1, n + 1):
+	if infected[i]:
+		print(1, end="")
+	else:
+		print(0, end="")
