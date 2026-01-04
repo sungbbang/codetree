27e0eaba3825fb5ae05e8c766304b99e81fe9253
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-04 17:32:06 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 75ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 260104/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -0,0 +1,26 @@
+n, m = map(int, input().split())
+grid = [[0] * m for _ in range(n)]
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < m
+
+dxs = [0, 1, 0, -1]
+dys = [1, 0, -1, 0]
+dir_num = 0
+x = y = 0
+grid[0][0] = 'A'
+for i in range(1, n * m):
+    nx = x + dxs[dir_num]
+    ny = y + dys[dir_num]
+
+    if not in_range(nx, ny) or grid[nx][ny] != 0:
+        dir_num = (dir_num + 1) % 4
+
+    x = x + dxs[dir_num]
+    y = y + dys[dir_num]
+
+    grid[x][y] = chr(ord('A') + (ord('A') + i) % ord('A'))
+
+
+for row in grid:
+    print(*row)
