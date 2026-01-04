c7ffa18819298b9393c45d996fd887012c5f5afe
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-04 20:23:44 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 64ms, Memory: 17MB, Status: Passed - Codetree

--- 260104/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
+++ 260104/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
@@ -5,43 +5,22 @@ board = [list(map(int, input().split())) for _ in range(n)]
 dxs = [0, 1, 0, -1]
 dys = [1, 0, -1, 0]
 
-mapper = {
-    'E': 0,
-    'S': 1,
-    'W': 2,
-    'N': 3
-}
-
 
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
 x = y = n//2
 total = board[x][y]
-move_dir = 'N'
+move_dir = 3
 for command in commands:
     if command == 'L':
-        if move_dir == 'N':
-            move_dir = 'W'
-        elif move_dir == 'S':
-            move_dir = 'E'
-        elif move_dir == 'E':
-            move_dir = 'N'
-        elif move_dir == 'W':
-            move_dir = 'S'
+        move_dir = (move_dir - 1 + 4) % 4
     elif command == 'R':
-        if move_dir == 'N':
-            move_dir = 'E'
-        elif move_dir == 'S':
-            move_dir = 'W'
-        elif move_dir == 'E':
-            move_dir = 'S'
-        elif move_dir == 'W':
-            move_dir = 'N'
+        move_dir = (move_dir + 1) % 4
     
     elif command == 'F':
-        nx = x + dxs[mapper[move_dir]]
-        ny = y + dys[mapper[move_dir]]
+        nx = x + dxs[move_dir]
+        ny = y + dys[move_dir]
         
         if in_range(nx, ny):
             total += board[nx][ny]
