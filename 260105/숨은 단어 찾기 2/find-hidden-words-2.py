b04617420d55e9dff6fad683f3401d196f9fa0b4
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-05 21:51:25 +0900

    Title: 숨은 단어 찾기 2, Time: 69ms, Memory: 17MB, Status: Passed - Codetree

--- 260105/숨은 단어 찾기 2/find-hidden-words-2.py
+++ 260105/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -1,34 +1,30 @@
 n, m = map(int, input().split())
 arr = [input() for _ in range(n)]
 
-dxs = [1, 1, 1, -1, -1, -1, 0, 0]
-dys = [-1, 0, 1, -1, 0, 1, -1, 1]
+dxs = [-1, -1, -1, 0, 0, 1, 1, 1]
+dys = [-1, 0, 1, -1, 1, -1, 0, 1]
 
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < m
 
+def is_correct(s):
+    return s == 'LEE' or s == 'EEL'
+
 ans = 0
 for i in range(n):
     for j in range(m):
-        if arr[i][j] != 'L':
-            continue
-
         for dx, dy in zip(dxs, dys):
-            cnt = 1
-            x, y = i, j
-            while True:
-                nx = x + dx
-                ny = y + dy
+            temp = ''
+            for k in range(3):
+                nx = i + dx * k
+                ny = j + dy * k
+
                 if not in_range(nx, ny):
                     break
-                
-                if arr[nx][ny] != 'E':
-                    break
-                
-                cnt += 1
-                x, y = nx, ny
-        
-            if cnt == 3:
+
+                temp += arr[nx][ny]
+
+            if is_correct(temp):
                 ans += 1
 
-print(ans)
+print(ans // 2)
