449e179b1b9e9351cc297934190398c749b1e7a1
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-05 21:51:57 +0900

    Title: 숨은 단어 찾기 2, Time: 72ms, Memory: 17MB, Status: Passed - Codetree

--- 260105/숨은 단어 찾기 2/find-hidden-words-2.py
+++ 260105/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -1,8 +1,8 @@
 n, m = map(int, input().split())
 arr = [input() for _ in range(n)]
 
-dxs = [-1, -1, -1, 0, 0, 1, 1, 1]
-dys = [-1, 0, 1, -1, 1, -1, 0, 1]
+dxs = [1, 1, 1, -1, -1, -1, 0, 0]
+dys = [-1, 0, 1, -1, 0, 1, -1, 1]
 
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < m
