9bb0977094184134ac6b48fe0a77bc14ecb88944
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-05 19:48:12 +0900

    Title: Carry 피하기 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260105/Carry 피하기 2/escaping-carry-2.py
+++ 260105/Carry 피하기 2/escaping-carry-2.py
@@ -2,8 +2,7 @@ n = int(input())
 arr = [int(input()) for _ in range(n)]
 
 def is_carry(a, b, c):
-    zero = 0
-    while zero < 2:
+    while True:
         if a % 10 + b % 10 + c % 10 >= 10:
             return True
         
@@ -11,9 +10,8 @@ def is_carry(a, b, c):
         b //= 10
         c //= 10
 
-        if a == 0: zero += 1
-        if b == 0: zero += 1
-        if c == 0: zero += 1
+        if a == 0 and b == 0 and c == 0:
+            break
 
     return False
 
@@ -22,7 +20,8 @@ ans = -1
 for i in range(n):
     for j in range(i+1, n):
         for k in range(j+1, n):
-            if not is_carry(arr[i], arr[j], arr[k]):
-                ans = max(ans, arr[i] + arr[j] + arr[k])
+            if is_carry(arr[i], arr[j], arr[k]):
+                continue
+            ans = max(ans, arr[i] + arr[j] + arr[k])
 
 print(ans)
