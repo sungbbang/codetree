a69569186e12fc6f0186bd42cb306183539afad1
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-05 21:01:54 +0900

    Title: 오목, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260105/오목/O-mok.py
+++ 260105/오목/O-mok.py
@@ -1,61 +1,36 @@
+import sys
 arr = [list(map(int, input().split())) for _ in range(19)]
 n = 19
 
-def is_same(arr):
-    one = two = 0
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
 
-    for num in arr:
-        if num == 1: one += 1
-        elif num == 2: two += 1
+dxs = [1, 1, 1, -1, -1, -1, 0, 0]
+dys = [-1, 0, 1, -1, 0, 1, -1, 1]
 
-    return True if one == 5 or two == 5 else False
-
-winner = 0
-r = c = -1
-# 세로로 연속적인 경우
-for i in range(n-4):
-    for j in range(n):
-        temp = []
-        for k in range(5):
-            temp.append(arr[i+k][j])
-        if is_same(temp):
-            winner = temp[0]
-            r = i+1+2
-            c = j+1
-# 가로로 연속적인 경우
 for i in range(n):
-    for j in range(n-4):
-        temp = []
-        for k in range(5):
-            temp.append(arr[i][j+k])
-        if is_same(temp):
-            winner = temp[0]
-            r = i+1
-            c = j+1+2
-# 대각(\)으로 연속적인 경우
-for i in range(n-4):
-    for j in range(n-4):
-        temp = []
-        for k in range(5):
-            temp.append(arr[i+k][j+k])
-        if is_same(temp):
-            winner = temp[0]
-            r = i+1+2
-            c = j+1+2
-# 대각(/)으로 연속적인 경우
-for i in range(n-4):
-    for j in range(4, n-4):
-        temp = []
-        for k in range(5):
-            temp.append(arr[i+k][j-k])
-        if is_same(temp):
-            winner = temp[0]
-            r = i+1+2
-            c = j+1-2
+    for j in range(n):
+        if arr[i][j] == 0:
+            continue
 
+        for dx, dy in zip(dxs, dys):
+            cnt = 1
+            x, y = i, j
+            for _ in range(5):
+                nx = x + dx
+                ny = y + dy
+                if not in_range(nx, ny):
+                    break
+                
+                if arr[nx][ny] != arr[x][y]:
+                    break
+                
+                cnt += 1
+                x, y = nx, ny
+        
+        if cnt == 5:
+            print(arr[i][j])
+            print(i + 2 * dx + 1, j + 2 * dy + 1)
+            sys.exit()
 
-if winner:
-    print(winner)
-    print(r, c)
-else:
-    print(0)
+print(0)
