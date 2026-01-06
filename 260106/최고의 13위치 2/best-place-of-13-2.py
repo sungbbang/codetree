df58415c2d019a966f48b039a84d44978f480ffd
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-06 15:16:48 +0900

    Title: 최고의 13위치 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260106/최고의 13위치 2/best-place-of-13-2.py
+++ 260106/최고의 13위치 2/best-place-of-13-2.py
@@ -3,8 +3,10 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 
 ans = []
 for i in range(n):
+    max_grid = 0
     for j in range(n-2):
-        ans.append(arr[i][j] + arr[i][j+1] + arr[i][j+2])
+        max_grid = max(max_grid, arr[i][j] + arr[i][j+1] + arr[i][j+2])
+    ans.append(max_grid)
     
 ans.sort(reverse=True)
 print(ans[0] + ans[1])
