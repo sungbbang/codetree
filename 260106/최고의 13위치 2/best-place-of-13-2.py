1dc0a9acc4e8262e2d8ff9210633fbe4697c98ea
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-06 15:15:51 +0900

    Title: 최고의 13위치 2, Time: 54ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260106/최고의 13위치 2/best-place-of-13-2.py
+++ 260106/최고의 13위치 2/best-place-of-13-2.py
@@ -1,22 +1,10 @@
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-ans = -1
-if n >= 6:
-    for i in range(n):
-        for j in range(n-2):
-            for k in range(n):
-                for l in range(j+3, n-2):
-                    grid1 = arr[i][j] + arr[i][j+1] + arr[i][j+2]
-                    grid2 = arr[k][l] + arr[k][l+1] + arr[k][l+2]
-                    ans = max(ans, grid1 + grid2)
-else:
-    for i in range(n):
-        for j in range(n-2):
-            for k in range(i+1, n):
-                for l in range(n-2):
-                    grid1 = arr[i][j] + arr[i][j+1] + arr[i][j+2]
-                    grid2 = arr[k][l] + arr[k][l+1] + arr[k][l+2]
-                    ans = max(ans, grid1 + grid2)
-
-print(ans)
+ans = []
+for i in range(n):
+    for j in range(n-2):
+        ans.append(arr[i][j] + arr[i][j+1] + arr[i][j+2])
+    
+ans.sort(reverse=True)
+print(ans[0] + ans[1])
