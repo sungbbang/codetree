58c726073f5f86561ae958e8117a62147cf68273
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-06 11:14:32 +0900

    Title: 최고의 13위치 2, Time: 52ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260106/최고의 13위치 2/best-place-of-13-2.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+ans = -1
+for i in range(n):
+    for j in range(n-2):
+        for k in range(i+1, n):
+            for l in range(n-2):
+                grid1 = arr[i][j] + arr[i][j+1] + arr[i][j+2]
+                grid2 = arr[k][l] + arr[k][l+1] + arr[k][l+2]
+                ans = max(ans, grid1 + grid2)
+
+print(ans)
