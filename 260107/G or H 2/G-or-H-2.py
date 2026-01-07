badef82fd7d5b0aafb8baaf2976dc017a1c23d6c
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-07 21:54:28 +0900

    Title: G or H 2, Time: 64ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 260107/G or H 2/G-or-H-2.py
@@ -0,0 +1,48 @@
+MAX = 100
+n = int(input())
+arr = [0] * (MAX+1)
+for _ in range(n):
+    pos, alpha = input().split()
+    arr[int(pos)] = alpha
+
+def check(arr):
+    g = h = zero = 0
+    
+    for el in arr:
+        if el == 'G':
+            g += 1
+        elif el == 'H':
+            h += 1
+        elif el == 0:
+            zero += 1
+    
+    if g == 0 and h == 0:
+        return False
+    
+    if g == h:
+        return True
+
+    if zero == 0:
+        if g > 0 and h == 0:
+            return True
+    
+        if g == 0 and h > 0:
+            return True
+    
+    return False
+
+ans = 0
+for i in range(MAX+1):
+    for j in range(i, MAX+1):
+        if j == 0: continue
+        temp = []
+        for k in range(i, j+1):
+            temp.append(arr[k])
+        
+        if temp[0] == 0 or temp[-1] == 0:
+            continue
+
+        if check(temp):
+            ans = max(ans, j-i)
+
+print(ans)
