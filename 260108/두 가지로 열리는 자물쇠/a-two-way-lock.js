2724c2620264d2dbe8b87b938e8dcf1387b20263
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-08 14:02:43 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 82ms, Memory: 10MB, Status: Passed - Codetree

--- 260108/두 가지로 열리는 자물쇠/a-two-way-lock.js
+++ 260108/두 가지로 열리는 자물쇠/a-two-way-lock.js
@@ -8,43 +8,18 @@ function inRange(num, target) {
     return Math.abs(num - target) <= 2 || n - Math.abs(num - target) <= 2;
 }
 
-const first = [];
-for (let num of [a1, b1, c1]) {
-    const temp = [];
-    for (let i = 1; i <= n; i++) {
-        if (inRange(num, i)) {
-            temp.push(i);
-        }
-    }
-    first.push(temp);
-}
-
-const second = [];
-for (let num of [a2, b2, c2]) {
-    const temp = [];
-    for (let i = 1; i <= n; i++) {
-        if (inRange(num, i)) {
-            temp.push(i);
-        }
-    }
-    second.push(temp);
-}
-
-let a_cnt = 0;
-let b_cnt = 0;
-let c_cnt = 0;
-for (let i = 0; i < 5; i++) {
-    for (let j = 0; j < 5; j++) {
-        if (first[0][i] === second[0][j]) {
-            a_cnt++;
-        }
-        if (first[1][i] === second[1][j]) {
-            b_cnt++;
-        }
-        if (first[2][i] === second[2][j]) {
-            c_cnt++;
+let count = 0;
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+        for (let k = 1; k <= n; k++) {
+            if (
+                inRange(i, a1) && inRange(j, b1) && inRange(k, c1) ||
+                inRange(i, a2) && inRange(j, b2) && inRange(k, c2)
+            ) {
+                count++;
+            }
         }
     }
 }
 
-console.log((5 ** 3) * 2 - (a_cnt * b_cnt * c_cnt))
+console.log(count);
