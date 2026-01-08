b158ecb3921f76a4e1f5954ca1dc0bd9be2c193b
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-08 13:59:35 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 64ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260108/두 가지로 열리는 자물쇠/a-two-way-lock.js
+++ 260108/두 가지로 열리는 자물쇠/a-two-way-lock.js
@@ -1,58 +1,50 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
-const first = input[1].split(' ').map(Number);
-const second = input[2].split(' ').map(Number);
+const [a1, b1, c1] = input[1].split(' ').map(Number);
+const [a2, b2, c2] = input[2].split(' ').map(Number);
 
-function inRange(num, x) {
-    return Math.abs(num - x) <= 2 || n - Math.abs(num - x) <= 2;
+function inRange(num, target) {
+    return Math.abs(num - target) <= 2 || n - Math.abs(num - target) <= 2;
 }
 
-const first_arr = [];
-const second_arr = [];
-
-for (let num of first) {
+const first = [];
+for (let num of [a1, b1, c1]) {
     const temp = [];
     for (let i = 1; i <= n; i++) {
         if (inRange(num, i)) {
             temp.push(i);
         }
     }
-    first_arr.push(temp);
+    first.push(temp);
 }
 
-for (let num of second) {
+const second = [];
+for (let num of [a2, b2, c2]) {
     const temp = [];
     for (let i = 1; i <= n; i++) {
         if (inRange(num, i)) {
             temp.push(i);
         }
     }
-    second_arr.push(temp);
+    second.push(temp);
 }
 
-const first_can = first_arr[0].length * first_arr[1].length * first_arr[2].length;
-const second_can = second_arr[0].length * second_arr[1].length * second_arr[2].length;
-let total = first_can + second_can;
-
+let a_cnt = 0;
+let b_cnt = 0;
+let c_cnt = 0;
 for (let i = 0; i < 5; i++) {
     for (let j = 0; j < 5; j++) {
-        if (first_arr[0][i] === second_arr[0][j]) {
-            for (let k = 0; k < 5; k++) {
-                for (let l = 0; l < 5; l++) {
-                    if (first_arr[1][k] === second_arr[1][l]) {
-                        for (let m = 0; m < 5; m++) {
-                            for (let n = 0; n < 5; n++) {
-                                if (first_arr[2][m] === second_arr[2][n]) {
-                                    total--;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+        if (first[0][i] === second[0][j]) {
+            a_cnt++;
+        }
+        if (first[1][i] === second[1][j]) {
+            b_cnt++;
+        }
+        if (first[2][i] === second[2][j]) {
+            c_cnt++;
         }
     }
 }
 
-console.log(total)
+console.log((5 ** 3) * 2 - (a_cnt * b_cnt * c_cnt))
