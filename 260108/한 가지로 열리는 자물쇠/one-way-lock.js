a4be2401d1e034b5de68f75a89ba1e1a9c0274c9
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-08 10:03:08 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 77ms, Memory: 11MB, Status: Wrong Answer - Codetree

--- 260108/한 가지로 열리는 자물쇠/one-way-lock.js
+++ 260108/한 가지로 열리는 자물쇠/one-way-lock.js
@@ -3,11 +3,13 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const [a, b, c] = input[1].split(' ').map(Number);
 
-let total = n ** 3;
+let total = 0;
 for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= n; j++) {
         for (let k = 1; k <= n; k++) {
-            if (i - 2 > a && j - 2 > b && k - 2 > c) total--;
+            if (i <= a + 2 || j <= b + 2 || k <= c + 2) {
+                total++;
+            }
         }
     }
 }
