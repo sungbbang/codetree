bae391758500abe06e8ce98f2b8669910e0dfec3
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-08 10:09:48 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 260108/한 가지로 열리는 자물쇠/one-way-lock.js
+++ 260108/한 가지로 열리는 자물쇠/one-way-lock.js
@@ -7,7 +7,7 @@ let total = 0;
 for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= n; j++) {
         for (let k = 1; k <= n; k++) {
-            if (i <= a + 2 || j <= b + 2 || k <= c + 2) {
+            if (Math.abs(i-a) <= 2 || Math.abs(j-b) <= 2 || Math.abs(k-c) <= 2) {
                 total++;
             }
         }
