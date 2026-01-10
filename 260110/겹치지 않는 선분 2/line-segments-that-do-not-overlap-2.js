343887ddb0a120949b335f58a839783fac10af8f
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-10 20:23:06 +0900

    Title: 겹치지 않는 선분 2, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 260110/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.js
+++ 260110/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.js
@@ -3,8 +3,9 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const arr = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
 
-let ans = n;
+let ans = 0;
 for (let i = 0; i < n; i++) {
+    let flag = true;
     for (let j = 0; j < n; j++) {
         if (i === j) continue;
 
@@ -12,9 +13,11 @@ for (let i = 0; i < n; i++) {
         const [s2, e2] = arr[j];
 
         if (s1 > s2 && e1 < e2 || s1 < s2 && e1 > e2) {
-            ans--;
+            flag = false;
+            break;
         }
     }
+    if (flag) ans++;
 }
 
 console.log(ans);
