c5d1f9e79de0bdf2d04fde7d62245193d69ba944
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-10 12:15:18 +0900

    Title: 삼각형 만들기, Time: 73ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 260110/삼각형 만들기/create-triangle.js
+++ 260110/삼각형 만들기/create-triangle.js
@@ -3,45 +3,50 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const points = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
 
-const MAX = 10000;
 function isRightTriangle(p1, p2, p3) {
-    let [x1, y1] = p1;
-    let [x2, y2] = p2;
-    let [x3, y3] = p3;
+    const [x1, y1] = p1;
+    const [x2, y2] = p2;
+    const [x3, y3] = p3;
 
-    x1 += MAX;
-    x2 += MAX;
-    x3 += MAX;
-    y1 += MAX;
-    y2 += MAX;
-    y3 += MAX;
+    // p1이 직각 꼭짓점
+    if ((x1 === x2 && y1 === y3) || (x1 === x3 && y1 === y2)) {
+        return true;
+    }
 
-    const dot1 = (x2 - x1) * (x3 - x1) + (y2 - y1) * (y3 - y1);
-    const dot2 = (x1 - x2) * (x3 - x2) + (y1 - y2) * (y3 - y2);
-    const dot3 = (x1 - x3) * (x2 - x3) + (y1 - y3) * (y2 - y3);
+    // p2가 직각 꼭짓점
+    if ((x2 === x1 && y2 === y3) || (x2 === x3 && y2 === y1)) {
+        return true;
+    }
 
-    return dot1 === 0 || dot2 === 0 || dot3 === 0;
+    // p3가 직각 꼭짓점
+    if ((x3 === x1 && y3 === y2) || (x3 === x2 && y3 === y1)) {
+        return true;
+    }
+
+    return false;
 }
 
 function getArea(p1, p2, p3) {
-    const maxX = Math.max(p1[0], p2[0], p3[0]);
-    const maxY = Math.max(p1[1], p2[1], p3[1]);
-    const minX = Math.min(p1[0], p2[0], p3[0]);
-    const minY = Math.min(p1[1], p2[1], p3[1]);
-
-    return (maxX - minX) * (maxY - minY) / 2;
+    const [x1, y1] = p1;
+    const [x2, y2] = p2;
+    const [x3, y3] = p3;
+    
+    const width = Math.max(x1, x2, x3) - Math.min(x1, x2, x3);
+    const height = Math.max(y1, y2, y3) - Math.min(y1, y2, y3);
+    
+    return width * height / 2;
 }
 
 let ans = 0;
 for (let i = 0; i < n; i++) {
-    for (let j = i+1; j < n; j++) {
-        for (let k = j+1; k < n; k++) {
-            const d1 = points[i];
-            const d2 = points[j];
-            const d3 = points[k];
+    for (let j = i + 1; j < n; j++) {
+        for (let k = j + 1; k < n; k++) {
+            const p1 = points[i];
+            const p2 = points[j];
+            const p3 = points[k];
 
-            if (isRightTriangle(d1, d2, d3)) {
-                ans = Math.max(ans, parseInt(getArea(d1, d2, d3)));
+            if (isRightTriangle(p1, p2, p3)) {
+                ans = Math.max(ans, parseInt(getArea(p1, p2, p3)));
             }
         }
     }
