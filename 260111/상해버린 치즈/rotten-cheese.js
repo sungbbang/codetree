a4eccee0d47df5adc8ab3f0d45674d21ee84fa3a
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-11 16:15:19 +0900

    Title: 상해버린 치즈, Time: 94ms, Memory: 13MB, Status: Passed - Codetree

--- 260111/상해버린 치즈/rotten-cheese.js
+++ 260111/상해버린 치즈/rotten-cheese.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const [N, M, D, S] = input[0].split(' ').map(Number);
 
-const whoEatCheeseWhen = [];
+const whoEatWhen = [];
 for (let i = 0; i < D; i++) {
     const [p, cheese, t] = input[i+1].split(' ').map(Number);
-    whoEatCheeseWhen.push([p, cheese, t]);
+    whoEatWhen.push([p, cheese, t]);
 }
 
 const whoSickWhen = [];
@@ -14,36 +14,35 @@ for (let i = 0; i < S; i++) {
     whoSickWhen.push([p, t]);
 }
 
-function getSickPerson(eat, sick) {
-    let result = [];
-    for (let [eatPerson, eatTime] of eat) {
-        for (let [sickPerson, sickTime] of sick) {
-            if (result.includes(eatPerson)) continue;
-
-            if (eatPerson === sickPerson) {
-                if (eatTime < sickTime) {
-                    result.push(eatPerson);
-                }
-            } else {
-                result.push(eatPerson);
+function isStale(cheeseId) {
+    for (let [sickPerson, sickTime] of whoSickWhen) {
+        let alreadySick = true;
+        for (let [eatPerson, cheese, eatTime] of whoEatWhen) {
+            if (cheese === cheeseId && eatPerson === sickPerson && eatTime < sickTime) {
+                alreadySick = false;
+                break;
             }
         }
+        if (alreadySick) {
+            return false;
+        }
     }
-    return result;
+    return true;
 }
 
 let ans = 0;
-// 치즈를 순회(치즈는 1번부터 시작)
 for (let i = 1; i <= M; i++) {
-    // 치즈 섭취 정보를 순회
-    const whoEatCheese = [];
-    for (let [person, cheese, time] of whoEatCheeseWhen) {
-        if (cheese === i) {
-            whoEatCheese.push([person, time]);
+    // 치즈가 상했으면
+    if (isStale(i)) {
+        const atePeople = [];
+        for (let j = 0; j < D; j++) {
+            if (whoEatWhen[j][1] !== i) continue;
+            if (atePeople.includes(whoEatWhen[j][0])) continue;
+            
+            atePeople.push(whoEatWhen[j][0]);
         }
+        ans = Math.max(ans, atePeople.length);  
     }
-
-    ans = Math.max(ans, getSickPerson(whoEatCheese, whoSickWhen).length)
 }
 
 console.log(ans);
