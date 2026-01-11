49c1cd9ae836de0e62fa7c742561c1bc6b9d602f
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-11 17:14:21 +0900

    Title: 개발자의 순위, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 260111/개발자의 순위/developers-rank.js
+++ 260111/개발자의 순위/developers-rank.js
@@ -1,16 +1,24 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const [k, n] = input[0].split(' ').map(Number);
-const arr = input.slice(1).map(v => v.split(' ').map(Number));
+
+const record = Array(k).fill(0).map(() => Array(n+1).fill(0));
+for (let i = 1; i <= k; i++) {
+    const match = input[i].split(' ').map(Number);
+    for (let j = 1; j <= n; j++) {
+        record[i-1][match[j-1]] = j;
+    }
+}
+
 let ans = 0;
-for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
         if (i === j) continue;
         let always = true;
         for (let l = 0; l < k; l++) {
-            if (arr[l][i] <= arr[l][j]) {
+            if (record[l][i] >= record[l][j]) {
                 always = false;
-                break
+                break;
             }
         }
         if (always) ans++;
