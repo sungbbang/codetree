4c7184f34c581e30a03a36d36f9609f1c8184da2
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-12 20:38:03 +0900

    Title: 스승의 은혜 3, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 260112/스승의 은혜 3/the-grace-form-teacher-3.js
+++ 260112/스승의 은혜 3/the-grace-form-teacher-3.js
@@ -1,35 +1,41 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, b] = input[0].split(' ').map(Number);
-const ps = input.slice(1).map(v => v.split(' ').map(Number));
+const presents = [];
 
-ps.sort((a, b) => {
-    const [pA, sA] = a;
-    const [pB, sB] = b;
-
-    if (pA < pB) return -1;
-    if (pA > pB) return 1;
-    if (pA === pB) {
-        if (sA < sB) return -1;
-        if (sA > sB) return 1;
+class Present {
+    constructor(price, deliveryFee) {
+        this.price = price;
+        this.deliveryFee = deliveryFee;
     }
-})
+}
+
+for (let i = 1; i <= n; i++) {
+    const [p, s] = input[i].split(' ').map(Number);
+    presents.push(new Present(p, s));
+}
 
 let ans = 0;
 for (let i = 0; i < n; i++) {
-    let total = ps[i][0] / 2 + ps[i][1];
-    let cnt = 1;
+    const present = presents[i];
+    let total = present.price / 2 + present.deliveryFee;
+    const arr = [total];
     for (let j = 0; j < n; j++) {
         if (i === j) continue;
 
-        if (total >= b) {
-            break;
-        }
+        arr.push(presents[j].price + presents[j].deliveryFee);
+    }
+    arr.sort((a, b) => a - b);
 
-        total = total + ps[j][0] + ps[j][1];
+    let cnt = 0;
+    let sum = 0;
+    for (let num of arr) {
+        if (sum >= b) break;
+        sum += num;
         cnt++;
     }
-    if (total > b) cnt--;
+    if (sum > b) cnt--;
+
     ans = Math.max(ans, cnt);
 }
 
