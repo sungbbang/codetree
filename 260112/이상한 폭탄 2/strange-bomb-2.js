4f9b58c853259acc576bf384cec8b7758a9433a8
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-12 16:29:12 +0900

    Title: 이상한 폭탄 2, Time: 69ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260112/이상한 폭탄 2/strange-bomb-2.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, k] = input[0].split(' ').map(Number);
+const bombs = input.slice(1).map(Number);
+
+let ans = -1;
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+        if (i === j) continue;
+        
+        const bomb1 = bombs[i];
+        const bomb2 = bombs[j];
+        if (bomb1 === bomb2 && Math.abs(i-j <= k)) {
+            ans = Math.max(ans, bomb1);
+        }
+    }
+}
+
+console.log(ans);
