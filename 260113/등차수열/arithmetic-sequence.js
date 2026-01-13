23d1388510aea3e610a13716059cab0cc83c5c9b
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-13 16:11:36 +0900

    Title: 등차수열, Time: 70ms, Memory: 10MB, Status: Passed - Codetree

--- 260113/등차수열/arithmetic-sequence.js
+++ 260113/등차수열/arithmetic-sequence.js
@@ -8,15 +8,15 @@ let ans = 0;
 for (let i = 1; i <= MAX; i++) {
     let cnt = 0;
     for (let j = 0; j < n; j++) {
-        for (let k = 0; k < n; k++) {
+        for (let k = j+1; k < n; k++) {
             if (j === k) continue;
 
-            if (arr[k] - i === i - arr[j]) {
+            if (arr[k] + arr[j] === 2*i) {
                 cnt++;
             }
         }
-        ans = Math.max(cnt/2, ans);
     }
+    ans = Math.max(cnt, ans);
 }
 
 console.log(ans);
