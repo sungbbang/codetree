05a92aba4f7d864275e944db4e6bcd2430ac1ab7
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-14 09:43:22 +0900

    Title: 좌표평면 위의 균형 2, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260114/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.js
+++ 260114/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.js
@@ -4,18 +4,9 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const points = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
 
-function getDiff(nums) {
-    let sum = 0;
-    for (let i = 0; i < 3; i++) {
-        sum += Math.abs(nums[i] - nums[i+1]);
-    }
-    return sum;
-}
-
-let minSum = Infinity;
-let ans = 0;
-for (let i = 2; i <= 100; i+=2) {
-    for (let j = 2; j <= 100; j+=2) {
+let ans = Infinity;
+for (let i = 2; i <= 14; i+=2) {
+    for (let j = 2; j <= 14; j+=2) {
         const count = [0, 0, 0, 0];
         for (let [x, y] of points) {
             if (x < i && y < j) count[0]++;
@@ -23,10 +14,8 @@ for (let i = 2; i <= 100; i+=2) {
             else if (x > i && y > j) count[2]++;
             else if (x < i && y > j) count[3]++;
         }
-        if (minSum > getDiff(count)) {
-            ans = Math.max(...count);
-        }
-        minSum = Math.min(minSum, getDiff(count));
+        const maxCount = Math.max(...count);
+        ans = Math.min(ans, maxCount);
     }
 }
 
