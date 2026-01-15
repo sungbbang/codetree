85ea2a9b1b77814e7c5ab138e5bd9112ff85c4dc
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-15 16:21:35 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 64ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260115/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.js
+++ 260115/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.js
@@ -4,10 +4,22 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const str = input[1];
 
-for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-        const curr = str.slice(i, j+1);
-        const remain = str.slice(j)
-        console.log(curr, remain)
+// 문자열 길이가 1일 때부터 n까지 체크
+let ans = 0;
+for (let i = 1; i <= n; i++) {
+    let flag = false;
+    for (let j = 0; j <= n-i; j++) {
+        const curr = str.slice(j, j+i);
+        const remain = str.slice(j+i-1);
+        if (remain.includes(curr)) {
+            flag = true;
+            ans = i;
+            break;
+        }
+    }
+    if (!flag) {
+        break;
     }
 }
+
+console.log(ans+1);
