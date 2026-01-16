33944090772a6ff1fc9fca2c3c8fd779b020b21f
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-16 16:21:31 +0900

    Title: 독서실의 거리두기 5, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 260116/독서실의 거리두기 5/study-cafe-keeping-distance-5.js
+++ 260116/독서실의 거리두기 5/study-cafe-keeping-distance-5.js
@@ -3,27 +3,28 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const seat = input[1].split('');
 
-// 사람 위치 저장
-const people = [];
-for (let i = 0; i < n; i++) {
-    if (seat[i] === '1') people.push(i);
+function getMinDist() {
+    let dist = n;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i+1; j < n; j++) {
+            if (seat[i] === '1' && seat[j] === '1') {
+                dist = Math.min(dist, Math.abs(j - i));
+            }
+        }
+    }
+
+    return dist;
 }
 
-let ans = 0;
 
+let ans = 0;
 for (let i = 0; i < n; i++) {
-    if (seat[i] === '1') continue;
-    
-    // i에 새 사람 추가 후 정렬
-    const newPeople = [...people, i].sort((a, b) => a - b);
-    
-    // 모든 인접 거리 중 최솟값 계산
-    let minDist = Infinity;
-    for (let j = 0; j < newPeople.length - 1; j++) {
-        minDist = Math.min(minDist, newPeople[j + 1] - newPeople[j]);
+    if (seat[i] === '0') {
+        seat[i] = '1';
+        ans = Math.max(ans, getMinDist());
+        seat[i] = '0';
     }
-    
-    ans = Math.max(ans, minDist);
 }
 
 console.log(ans);
