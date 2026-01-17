34f22b873a6dee8c8b57defbe97be0ab076c908c
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-17 17:26:11 +0900

    Title: 수들의 최대 차, Time: 69ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260117/수들의 최대 차/maximum-difference-in-numbers.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, k] = input[0].split(' ').map(Number);
+const numbers = input.slice(1).map(v => v.split(' ').map(Number));
+
+numbers.sort((a, b) => a - b);
+
+let ans = 0;
+for (let i = 0; i < n; i++) {
+    const temp = [numbers[i]];
+    for (let j = i+1; j < n; j++) {
+        if (numbers[j] - numbers[0] <= k) {
+            temp.push(numbers[j]);
+        } else {
+            break;
+        }
+    }
+    ans = Math.max(ans, temp.length);
+}
+
+console.log(ans);
