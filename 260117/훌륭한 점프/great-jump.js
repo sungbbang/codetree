c63eba913c7aea468552a2e1d6b039a398430cab
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-17 20:00:37 +0900

    Title: 훌륭한 점프, Time: 64ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260117/훌륭한 점프/great-jump.js
+++ 260117/훌륭한 점프/great-jump.js
@@ -21,16 +21,13 @@ function isPossible(maxVal) {
 }
 
 
-let ans = Infinity;
-if (k === 1) {
-    ans = Math.max(...stones);
-} else {
-    for (let i = Math.max(stones[0], stones[n-1]); i <= n; i++) {
-        if (isPossible(i)) {
-            
-            ans = Math.min(ans, i);
-        }
+let ans = [];
+// 1번과 n번 돌은 무조건 밟기 때문에 초기값은 둘중 가장 큰 값으로 설정
+for (let i = Math.max(stones[0], stones[n-1]); i <= n; i++) {
+    if (isPossible(i)) {
+        ans.push(i)
     }
 }
 
-console.log(ans);
+console.log(Math.min(...ans));
+
