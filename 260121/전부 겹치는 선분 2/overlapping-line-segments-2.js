b80dcdc086bcdfe244be6a66ae0ba30b16b70611
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-21 12:25:34 +0900

    Title: 전부 겹치는 선분 2, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 260121/전부 겹치는 선분 2/overlapping-line-segments-2.js
+++ 260121/전부 겹치는 선분 2/overlapping-line-segments-2.js
@@ -4,44 +4,31 @@ const n = parseInt(input[0]);
 const segments = input.slice(1).map(v => v.trim().split(' ').map(Number));
 const arr = Array(101).fill(0);
 
-for (let [x1, x2] of segments) {
-    for (let i = x1; i <= x2; i++) {
-        arr[i] += 1;
-    }
-}
+let ans = false;
 
-function check(line) {
-    const [x1, x2] = line;
+for (let skip = 0; skip < n; skip++) {
+    let maxX1 = 0;
+    let minX2 = Infinity;
+    let possible = false;
 
-    for (let i = x1; i <= x2; i++) {
-        arr[i]--;
-    }
+    for (let i = 0; i < n; i++) {
+        if (i === skip) continue;
 
-    let result = false;
-    for (let num of arr) {
-        if (num >= n-1) {
-            result = true;
-            break;
-        }
+        maxX1 = Math.max(maxX1, segments[i][0]);
+        minX2 = Math.min(minX2, segments[i][1]);
     }
 
-    for (let i = x1; i <= x2; i++) {
-        arr[i]++;
+    if (minX2 >= maxX1) {
+        possible = true;
     }
 
-    return result;
-
-}
-
-let flag = false;
-for (let i = 0; i < n; i++) {
-    if (check(segments[i])) {
-        flag = true;
+    if (possible) {
+        ans = true;
         break;
     }
 }
 
-if (flag) {
+if (ans) {
     console.log('Yes');
 } else {
     console.log('No');
