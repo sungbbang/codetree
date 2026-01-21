ed3cf3841621505bfc26aaa4118c1055a973635e
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-21 09:14:27 +0900

    Title: 구역 청소, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

--- 260121/구역 청소/district-cleaning.js
+++ 260121/구역 청소/district-cleaning.js
@@ -3,7 +3,20 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [a, b] = input[0].split(' ').map(Number);
 const [c, d] = input[1].split(' ').map(Number);
 
-const start = Math.min(a, b, c, d);
-const end = Math.max(a, b, c, d);
+const MAX = Math.max(a, b, c, d)
+const arr = Array(MAX+1).fill(0);
 
-console.log(end - start);
+for (let i = a; i < b; i++) {
+    arr[i]++;
+}
+
+for (let i = c; i < d; i++) {
+    arr[i]++;
+}
+
+let ans = 0;
+for (let num of arr) {
+    if (num >= 1) ans++;
+}
+
+console.log(ans);
