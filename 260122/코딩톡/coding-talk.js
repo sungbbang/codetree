360f65eefda242c19f393122d750d55230eb96f9
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-22 13:38:49 +0900

    Title: 코딩톡, Time: 61ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260122/코딩톡/coding-talk.js
+++ 260122/코딩톡/coding-talk.js
@@ -1,30 +1,28 @@
-const fs = require('fs');
-const input = fs.readFileSync(0).toString().trim().split('\n');
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
 const [n, m, p] = input[0].split(' ').map(Number);
-const messages = input.slice(1).map(v => v.split(' '));
+const messages = input.slice(1, 1 + m).map(line => line.split(' '));
 
+// p번째 이후 메시지 보낸 사람들
+const definitelyRead = new Set();
+for (let i = p-1; i < m; i++) {
+    definitelyRead.add(messages[i][0]);
+}
 
-const reads = [];
-for (let i = 0; i < m; i++) {
-    if (i >= p - 1) {
-        reads.push(messages[i][0]);
+// 안 읽은 가능성 있는 사람들
+const maybeNotRead = [];
+for (let i = 0; i < n; i++) {
+    const person = String.fromCharCode('A'.charCodeAt() + i);
+    if (!definitelyRead.has(person)) {
+        maybeNotRead.push(person);
     }
 }
 
-const A = 'A'.charCodeAt();
-for (let i = A; i < A + n; i++) {
-    let flag = false;
-    for (let read of reads) {
-        if (read === String.fromCharCode(i)) {
-            flag = true;
-            break;
-        }
-    }
-    if (flag) {
-        continue;
-    }
-    else {
-        process.stdout.write(String.fromCharCode(i) + ' ');
+let notReadCount = Number(messages[p-1][1]);
+// p번 메시지를 안 읽은 사람이 0보다 크다면
+if (notReadCount > 0) {
+    // 안 읽은 사람들이 모두 가능성이 있음
+    for (let person of maybeNotRead) {
+        process.stdout.write(person + ' ');
     }
 }
-
