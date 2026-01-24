02cd9ef9105888143aa2403092607bcd8af09f19
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-24 14:21:48 +0900

    Title: 독서실의 거리두기 3, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 260124/독서실의 거리두기 3/study-cafe-keeping-distance-3.js
+++ 260124/독서실의 거리두기 3/study-cafe-keeping-distance-3.js
@@ -3,16 +3,23 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const seats = input[1].split("").map(Number);
 
-let ans = 0;
+let maxDiff = 0;
+let minDiff = Infinity;
 for (let i = 0; i < n; i++) {
     if (seats[i] === 1) {
         for (let j = i+1; j < n; j++) {
             if (seats[j] === 1) {
-                ans = Math.max(ans, j - i);
+                if (j - i === 1) {
+                    console.log(1);
+                    process.exit();
+                }
+                minDiff = Math.min(minDiff, j - i);
+                maxDiff = Math.max(maxDiff, j - i);
                 break;
             }
         }
     }
 }
 
-console.log(Math.floor(ans / 2));
+minDiff = Math.min(minDiff, Math.floor(maxDiff/2));
+console.log(minDiff);
