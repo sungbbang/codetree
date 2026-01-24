dff222f0772b33d6a88482ae16b420c6457b00a8
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-24 22:18:49 +0900

    Title: 개발자의 가위바위보, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 260124/개발자의 가위바위보/developers-rock-paper-scissors.js
+++ 260124/개발자의 가위바위보/developers-rock-paper-scissors.js
@@ -1,34 +1,40 @@
-const fs = require('fs');
-const input = fs.readFileSync(0).toString().trim().split('\n');
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
 
+// 변수 선언 및 입력
 const n = Number(input[0]);
-const games = input.slice(1).map(v => v.split(' ').map(Number));
+const arr = input.slice(1).map((v) => v.split(" ").map(Number));
 
-// 0: 가위, 1: 바위, 2: 보
-const perms = [
-  [0, 1, 2],
-  [0, 2, 1],
-  [1, 0, 2],
-  [1, 2, 0],
-  [2, 0, 1],
-  [2, 1, 0],
-];
+let maxWin = 0;
 
-let answer = 0;
-
-for (const p of perms) {
-  let winCount = 0;
-
-  for (const [a, b] of games) {
-    const A = p[a - 1];  // 첫 번째 개발자
-    const B = p[b - 1];  // 두 번째 개발자
-
-    if ((B + 1) % 3 === A) {
-      winCount++;
+// Case 1. 1이 2를 이기고, 2가 3을 이기고 3이 1을 이기는 경우
+let win = 0;
+for (let i = 0; i < n; i++) {
+    const [a, b] = arr[i];
+    if (a === 1 && b === 2) {
+        win += 1;
+    } else if (a === 2 && b === 3) {
+        win += 1;
+    } else if (a === 3 && b === 1) {
+        win += 1;
     }
-  }
-
-  answer = Math.max(answer, winCount);
 }
 
-console.log(answer);
+maxWin = Math.max(maxWin, win);
+
+// Case 2. 1이 3을 이기고, 3이 2를 이기고 2가 1을 이기는 경우
+win = 0;
+for (let i = 0; i < n; i++) {
+    const [a, b] = arr[i];
+    if (a === 1 && b === 3) {
+        win += 1;
+    } else if (a === 3 && b === 2) {
+        win += 1;
+    } else if (a === 2 && b === 1) {
+        win += 1;
+    }
+}
+
+maxWin = Math.max(maxWin, win);
+
+console.log(maxWin);
