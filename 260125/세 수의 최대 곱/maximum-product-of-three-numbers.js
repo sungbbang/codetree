4964cc8e3117b2b1ac6a0c455684853ad7cac43b
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-25 21:50:24 +0900

    Title: 세 수의 최대 곱, Time: 68ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260125/세 수의 최대 곱/maximum-product-of-three-numbers.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input[1].split(' ').map(Number);
+
+const positive = [];
+const negative = [];
+let zero = false;
+
+for (let num of arr) {
+    if (num > 0) positive.push(num);
+    else if (num < 0) negative.push(num);
+    else zero = true;
+}
+
+positive.sort((a, b) => b - a);
+negative.sort((a, b) => a - b);
+
+let ans = -Infinity;
+// 양수가 3개 이상일 때
+let p3 = 0;
+if (positive.length >= 3) {
+    p3 = positive[0] * positive[1] * positive[2];
+}
+
+// 양수 1개, 음수 2개일 때
+let p1n2 = 0;
+if (positive.length >= 1 && negative.length >= 2) {
+    p1n2 = positive[0] * negative[0] * negative[1];
+}
+
+// 양수 2개, 음수 1개일 때
+let p2n1 = 0;
+if (positive.length >= 2 && negative.length >= 1) {
+    let lastP = positive.length - 1;
+    let lastN = negative.length - 1;
+    p2n1 = positive[lastP] * positive[lastP-1] * negative[lastN];
+}
+
+// 음수 3개일 때
+let n3 = 0;
+if (negative.length >= 3) {
+    let lastN = negative.length - 1;
+    n3 = negative[lastN] * negative[lastN-1] * negative[lastN-2];
+}
+
+console.log(Math.max(zero, p3, p1n2, p2n1, n3));
