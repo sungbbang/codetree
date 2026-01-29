4acbcd002654e5f843c2a72687eddf3e90202fe2
sungbbang <96762809+sungbbang@users.noreply.github.com>
2026-01-29 14:00:17 +0900

    Title: 두 점으로 만드는 정사각형, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 260129/두 점으로 만드는 정사각형/square-made-of-two-points.js
+++ 260129/두 점으로 만드는 정사각형/square-made-of-two-points.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
-const [x1, y1, x2, y2] = input[0];
-const [a1, b1, a2, b2] = input[1];
+const [x1, y1, x2, y2] = input[0].split(' ').map(Number);
+const [a1, b1, a2, b2] = input[1].split(' ').map(Number);
 
 const minX = Math.min(x1, x2, a1, a2);
 const maxX = Math.max(x1, x2, a1, a2);
-const miny = Math.min(y1, b1, y2, b2); 
-const maxy = Math.max(y1, b1, y2, b2);
+const minY = Math.min(y1, b1, y2, b2); 
+const maxY = Math.max(y1, b1, y2, b2);
 
 const w = maxX - minX;
-const h = maxy - miny;
+const h = maxY - minY;
 
 console.log(Math.max(w, h) ** 2);
